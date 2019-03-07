99f436769430919380e4fe03a78fe12dbd75ef15
Erik Petigura <petigura@caltech.edu>
2019-03-06 17:58:36 -0800

    second refactoring that keep track of individual programs

--- sim/sim.py
+++ sim/sim.py
@@ -64,12 +64,7 @@ def load_candidates():
 
 class Sample(object):
     def __init__(self, df):
-        self.df = df 
-
-    def apply_global_cuts(self):
-        df = self.df.copy()
-        filters = """_DEJ2000 > 0"""
-        return df.query(filters)
+        self.df = df.copy() # protect the size/order of input
 
     def get_sample_statistics(self):
         sample = self.get_sample()
@@ -86,53 +81,82 @@ num_targets = {}
 class SampleBright(Sample):
     name = 'bright'
     description = 'Brightest 50 sun-like stars'
-    def get_sample(self):
-        df = self.apply_global_cuts()
-        filters = """Teff < 6100 and Vmag < 13.0 and Kp > 2.0 and Rs < 1.5 and Rp < 12 and  Ksig > 4.0"""
-        df = df.query(filters).sort_values(by='Vmag').iloc[:50]
-        return df 
-
+    def in_sample(self):
+        b = pd.Series(False, index=self.df.index) 
+        filters = """_DEJ2000 > 0 and Teff < 6100 and Vmag < 13.0 and Kp > 2.0 and Rs < 1.5 and Rp < 12 and  Ksig > 4.0"""
+        idx = self.df.query(filters).sort_values(by='Vmag').iloc[:50].index
+        b.loc[idx] = True
+        return b
 
 ## Perhaps we want to turn this sample into an M-dwarf selection
 class SampleClose(Sample):
     name = 'close'
     description = '10 closest dwarfs that are bighter than V < 13'
-    def get_sample(self):
-        df = self.apply_global_cuts()
-        df = df.query('Vmag < 13 and Rs < 1.5').sort_values(by='Dist').iloc[:10]
-        return df 
+    def in_sample(self):
+        df = self.df.copy()
+        b = pd.Series(False, index=self.df.index) 
+        idx = self.df.query('_DEJ2000 > 0 and Vmag < 13 and Rs < 1.5').sort_values(by='Dist').iloc[:10].index
+        b.loc[idx] = True
+        return b
 
 class SampleMulti(Sample):
     name = 'multi'
     description = 'Brightest 10 multis'
-    def get_sample(self):
-        df = self.apply_global_cuts()
-        df = df.query('Npl > 1').sort_values(by='Vmag').iloc[:10]
-        return df 
+    def in_sample(self):
+        b = pd.Series(False, index=self.df.index) 
+        idx = self.df.query('Npl > 1').sort_values(by='Vmag').iloc[:10].index
+        b.loc[idx] = True
+        return b
 
 class SampleUSP(Sample):
     name = 'usp'
     description = 'Brightest 5 USPs'
-    def get_sample(self):
-        df = self.apply_global_cuts()
-        df = df.query('Perp < 1.5 and Rp < 2').sort_values(by='Vmag').iloc[:5]
-        return df 
+    def in_sample(self):
+        b = pd.Series(False, index=self.df.index) 
+        idx = self.df.query('Perp < 1.5 and Rp < 2').sort_values(by='Vmag').iloc[:5].index
+        b.loc[idx] = True
+        return b
 
 TKS_Samples = [SampleBright, SampleClose, SampleMulti, SampleUSP]
-
-class SampleTKS(Sample):
-    name = 'tks'
-    description = 'Total sample'
-    def get_sample(self,verbose=False):
-        tks = []
-        for Sample in TKS_Samples:
-            s = Sample(self.df) 
-            if verbose:
-                print('adding')
-                print(s.get_sample_statistics())
-            tks.append(s.get_sample())
-
-        tks = pd.concat(tks)
-        tks = tks.drop_duplicates()
-        return tks
+def combine_samples(df):
+    df['inany'] = False
+    innames = []
+    names = []
+    for Sample in TKS_Samples:
+        s = Sample(df) 
+        names.append(s.name)
+        inname = 'in'+s.name
+        df[inname] = s.in_sample()
+        df['inany'] = df['inany'] | df[inname]
+        innames.append(inname)
     
+    df['nprograms'] = df[innames].sum(axis=1)
+    for name in names:
+        inname = 'in'+ name
+        sample = df[df[inname]]
+        print('in ' + name)
+        print(get_sample_statistics(sample))
+
+        print('only in ' + name)
+        sample = df[df[inname] & (df['nprograms']==1)]
+        print(get_sample_statistics(sample))
+
+    sample = df[df['inany']]
+    print('-'*80)
+    print('Total')
+    print(get_sample_statistics(sample))
+
+    print('Program overlap')
+    print(df.groupby('nprograms').size())
+    return df
+
+def get_sample_statistics(sample):
+    nplanets = len(sample)
+    sample = sample.groupby('TICID').first() # only count stars once
+    nnights = sample.exptime.sum() / (10*3600)
+    nstars = len(sample)
+    s = """\
+nstars = {:.0f}, nplanets={:.0f}, nnights = {:.1f},
+    """.format(nstars,nplanets,nnights)
+    return s
+
